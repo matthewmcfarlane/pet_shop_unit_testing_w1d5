8f70b32134d5da6c11435d77f3e02d5412fc8de3
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-12 13:06:26 +0000

    13 tasks passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -42,3 +42,6 @@ def remove_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
             pet_shop["pets"].remove(pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
