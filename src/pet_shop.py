c6ee83bef1bd4466a0869769fa19e5fb5a5f38fb
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-12 11:54:50 +0000

    5th test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -10,5 +10,10 @@ def add_or_remove_cash(pet_shop, cash):
     pet_shop ["admin"]["total_cash"] += cash
 
 
-def get_pets_sold(pet_shop)
-    
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, sold):
+    pet_shop["admin"]["pets_sold"] += sold
+
+
