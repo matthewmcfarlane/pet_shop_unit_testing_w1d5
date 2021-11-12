7a77704dea4e4c5da8014bd46940f62dde6c1ae4
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-12 12:41:24 +0000

    10th test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -31,4 +31,8 @@ def get_pets_by_breed(pet_shop, breed):
             
 
 
-
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    return None
