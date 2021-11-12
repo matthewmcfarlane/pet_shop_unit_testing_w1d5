b014adf84a18a7234c716e38a8a4f7dde803baff
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-12 11:45:43 +0000

    first 4 tests passed

---
+++ src/pet_shop.py
@@ -0,0 +1,14 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(total_cash):
+    return total_cash["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop ["admin"]["total_cash"] += cash
+
+
+def get_pets_sold(pet_shop)
+    
